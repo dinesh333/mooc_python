0f1beefe650d1125c98869b8ae07d562f2b7fbd0
dinesh333 <dinesh123basnet@gmail.com>
2021-12-26 20:51:50 -0500

    adding exercises for part 9

---
+++ mooc-programming-21/part09-01_fastest_car/src/fastest_car.py
@@ -0,0 +1,11 @@
+# Write your solution after the class Car
+# Do not make changes to the class!
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+   def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+# WRITE YOUR SOLUTION HERE:
