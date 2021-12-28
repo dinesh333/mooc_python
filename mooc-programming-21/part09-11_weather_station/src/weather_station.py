34266f855e72e66bb2a9c38db59be6d5bc8085a5
dinesh333 <dinesh123basnet@gmail.com>
2021-12-27 23:55:40 -0500

    added missing requirements for exercises in part 9

--- mooc-programming-21/part09-11_weather_station/src/weather_station.py
+++ mooc-programming-21/part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,30 @@
+# Please create a class named WeatherStation which is used to store observations about the weather. The class should have the following public attributes:
+
+# a constructor which takes the name of the station as its argument
+# a method named add_observation(observation: str) which adds an observation as the last entry in a list
+# a method named latest_observation() which returns the latest observation added to the list. If there are no observations yet, the method should return an empty string.
+# a method named number_of_observations() which returns the total number of observations added
+# a __str__ method which returns the name of the station and the total number of observations added as per the example below.
+# All attributes should be encapsulated, so they can't be directly accessed. It is up to you how you implement the class, as long as the public interface is exactly as described above.
+
+# An example of how the class is used:
+
+# station = WeatherStation("Houston")
+# station.add_observation("Rain 10mm")
+# station.add_observation("Sunny")
+# print(station.latest_observation())
+
+# station.add_observation("Thunderstorm")
+# print(station.latest_observation())
+
+# print(station.number_of_observations())
+# print(station)
+# Sample output
+# Sunny
+# Thunderstorm
+# 3
+# Houston, 3 observations
+
 # WRITE YOUR SOLUTION HERE:
 class WeatherStation:
     def __init__(self, name):
--- mooc-programming-21/part09-14_list_helper/src/list_helper.py
+++ mooc-programming-21/part09-14_list_helper/src/list_helper.py
@@ -1,3 +1,16 @@
+# Please create a class named ListHelper which contains the following two class methods.
+
+# greatest_frequency(my_list: list) returns the most common item on the list
+# doubles(my_list: list) returns the number of unique items which appear at least twice on the list
+# It should be possible to use these methods without creating an instance of the class. An example of how the methods could be used:
+
+# numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+# print(ListHelper.greatest_frequency(numbers))
+# print(ListHelper.doubles(numbers))
+# Sample output
+# 5
+# 3
+
 class ListHelper:
 
     @classmethod
