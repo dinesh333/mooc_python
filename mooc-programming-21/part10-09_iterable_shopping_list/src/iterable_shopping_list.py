eec2ac4c83b28432d62e7fda0e618676aa334d1d
dinesh333 <dinesh123basnet@gmail.com>
2021-12-29 18:59:57 -0500

    Completed simple date and iterable object exercises in part 10. Completed all exercises.

--- mooc-programming-21/part10-09_iterable_shopping_list/src/iterable_shopping_list.py
+++ mooc-programming-21/part10-09_iterable_shopping_list/src/iterable_shopping_list.py
@@ -16,7 +16,7 @@
 
 ####################################################################################################################################
 ####################################################################################################################################
-# TEE RATKAISUSI TÄHÄN:
+
 class ShoppingList:
     def __init__(self):
         self.products = []
@@ -32,3 +32,15 @@ class ShoppingList:
 
     def number(self, n: int):
         return self.products[n - 1][1]
+
+    def __iter__(self):
+        self.iterator_index = 0
+        return self
+    
+    def __next__(self):
+        if self.iterator_index < len(self.products):
+            product = self.products[self.iterator_index]
+            self.iterator_index += 1
+            return product
+        else:
+            raise StopIteration
