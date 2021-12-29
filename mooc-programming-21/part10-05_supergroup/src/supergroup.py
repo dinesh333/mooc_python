03570e0eaa0b56666d0bb42f1247f86ab821dc26
dinesh333 <dinesh123basnet@gmail.com>
2021-12-28 21:36:41 -0500

    Completed all exercises for part 10, section 5 and 6

--- mooc-programming-21/part10-05_supergroup/src/supergroup.py
+++ mooc-programming-21/part10-05_supergroup/src/supergroup.py
@@ -32,3 +32,35 @@ class SuperHero:
 
     def __str__(self):
         return f'{self.name}, superpowers: {self.superpowers}'
+
+class SuperGroup:
+    def __init__(self, name: str, location: str):
+        self._name = name
+        self._location = location
+        self._members = []
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def location(self):
+        return self._location
+    
+    def add_member(self, hero: SuperHero):
+        self._members.append(hero)
+    
+    def print_group(self):
+        print(f'{self._name}, {self._location}')
+        print('Members:')
+        for member in self._members:
+            print(member)
+
+if __name__ == '__main__':
+    superperson = SuperHero("SuperPerson", "Superspeed, superstrength")
+    invisible = SuperHero("Invisible Inca", "Invisibility")
+    revengers = SuperGroup("Revengers", "Emerald City")
+
+    revengers.add_member(superperson)
+    revengers.add_member(invisible)
+    revengers.print_group()
