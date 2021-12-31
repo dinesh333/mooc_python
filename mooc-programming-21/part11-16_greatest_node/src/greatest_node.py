a2e30907b28b181b79feb9e68e73e915fadae200
dinesh333 <dinesh123basnet@gmail.com>
2021-12-31 15:27:34 -0500

    Completed part 11, exercises 16 and 17. Exercise 16 finds greatest node in a binary tree using recursion. Exercise 17 uses recursion to find subordinates for bosses

--- mooc-programming-21/part11-16_greatest_node/src/greatest_node.py
+++ mooc-programming-21/part11-16_greatest_node/src/greatest_node.py
@@ -1,7 +1,54 @@
-# WRITE YOUR SOLUTION HERE:
+# Please write a function named greatest_node(root: Node) which takes the root node of a binary tree as its argument.
+
+# The function should return the node with the greatest value within the tree. The tree should be traversed recursively.
+
+# Hint: the function sum_of_nodes in the example above may come in handy.
+
+# An example of how the function should work:
+
+# if __name__ == "__main__":
+#     tree = Node(2)
+
+#     tree.left_child = Node(3)
+#     tree.left_child.left_child = Node(5)
+#     tree.left_child.right_child = Node(8)
+
+#     tree.right_child = Node(4)
+#     tree.right_child.right_child = Node(11)
+
+#     print(greatest_node(tree))
+# Sample output
+# 11
+
 class Node:
     """ Class is modeling single node in binary tree """
     def __init__(self, value, left_child:'Node' = None, right_child:'Node' = None):
         self.value = value
         self.left_child = left_child
         self.right_child = right_child
+
+def greatest_node(root: Node):
+    greatest = root.value
+
+    if root.left_child is not None:
+        left = greatest_node(root.left_child)
+        if left > greatest:
+            greatest = left
+    if root.right_child is not None:
+        right = greatest_node(root.right_child)
+        if right > greatest:
+            greatest = right
+
+    return greatest
+
+if __name__ == "__main__":
+    tree = Node(2)
+
+    tree.left_child = Node(3)
+    tree.left_child.left_child = Node(5)
+    tree.left_child.right_child = Node(8)
+
+    tree.right_child = Node(4)
+    tree.right_child.right_child = Node(11)
+
+    print(greatest_node(tree))
--- mooc-programming-21/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
+++ mooc-programming-21/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
@@ -1,4 +1,36 @@
-# WRITE YOUR SOLUTION HERE:
+# The class Employee models an employee of a company:
+
+# class Employee:
+#     def __init__(self, name: str):
+#         self.name = name
+#         self.subordinates = []
+
+#     def add_subordinate(self, employee: 'Employee'):
+#         self.subordinates.append(employee)
+# Please write a function named count_subordinates(employee: Employee) which recursively counts the number of subordinates each employee has.
+
+# An example of the function in action:
+
+# if __name__ == "__main__":
+#     t1 = Employee("Sally")
+#     t2 = Employee("Eric")
+#     t3 = Employee("Matthew")
+#     t4 = Employee("Emily")
+#     t5 = Employee("Adele")
+#     t6 = Employee("Claire")
+#     t1.add_subordinate(t4)
+#     t1.add_subordinate(t6)
+#     t4.add_subordinate(t2)
+#     t4.add_subordinate(t3)
+#     t4.add_subordinate(t5)
+#     print(count_subordinates(t1))
+#     print(count_subordinates(t4))
+#     print(count_subordinates(t5))
+# Sample output
+# 5
+# 3
+# 0
+
 class Employee:
     def __init__(self, name: str):
         self.name = name
@@ -6,3 +38,26 @@ class Employee:
 
     def add_subordinate(self, employee: 'Employee'):
         self.subordinates.append(employee)
+
+def count_subordinates(employee: Employee):
+    sub = 0
+    for emp in employee.subordinates:
+        sub += 1
+        sub += count_subordinates(emp)
+    return sub
+
+if __name__ == "__main__":
+    t1 = Employee("Sally")
+    t2 = Employee("Eric")
+    t3 = Employee("Matthew")
+    t4 = Employee("Emily")
+    t5 = Employee("Adele")
+    t6 = Employee("Claire")
+    t1.add_subordinate(t4)
+    t1.add_subordinate(t6)
+    t4.add_subordinate(t2)
+    t4.add_subordinate(t3)
+    t4.add_subordinate(t5)
+    print(count_subordinates(t1))
+    print(count_subordinates(t4))
+    print(count_subordinates(t5))
